b6054f3a15db02058d6e1bf0bfc708361ca7ff56
noil <a.bunitsky@gmail.com>
2020-01-13 19:50:23 +0300

    added total method

---
+++ errors.go
@@ -0,0 +1,15 @@
+package tea
+
+import "fmt"
+
+type APIError struct {
+	Errors []string `json:"errors"`
+}
+
+func (e APIError) Error() string {
+	if len(e.Errors) > 0 {
+		err := e.Errors[0]
+		return fmt.Sprintf("twitter: %v", err)
+	}
+	return ""
+}
---
+++ total_success.go
@@ -0,0 +1,3 @@
+package tea
+
+type Success map[string]interface{}
